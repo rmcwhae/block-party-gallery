6cdf9340187dd86882776285b95154d37918ac34
Russell McWhae <russell.mcwhae@gmail.com>
2023-01-30 19:47:56 -0800

    List of images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,11 @@
 
 	$: images = data.images;
 
-	console.log('images', images);
+	// $: console.log('images', images);
 </script>
 
 <h1>Gallery</h1>
+
+{#each images as image}
+	<p>{image.name}</p>
+{/each}
