4c81000d97c789ed365f65df226124b26eed4c66
Russell McWhae <russell.mcwhae@gmail.com>
2023-01-30 19:59:50 -0800

    Basic gallery view

--- src/lib/images.js
+++ src/lib/images.js
@@ -1,7 +1,9 @@
-export const images = Object.entries(import.meta.globEager('/content/*.json')).map(
+export const images = Object.entries(import.meta.globEager('/static/content/*.json')).map(
 	([path, data]) => {
+		const name = path.replace('static/content/', '').replace('.json', '');
 		return {
-			name: path.replace('/content/', '').replace('.json', ''),
+			name,
+			href: '/content' + name + '.png',
 			...data.default
 		};
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,20 @@
 	export let data;
 
 	$: images = data.images;
-
-	// $: console.log('images', images);
 </script>
 
 <h1>Gallery</h1>
 
-{#each images as image}
-	<p>{image.name}</p>
-{/each}
+<div class="grid">
+	{#each images as image}
+		<img src={image.href} alt={image.name} width={200} height={200} />
+	{/each}
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, 200px);
+		grid-gap: 1rem;
+	}
+</style>
