2e0592349671cd03ea5e267164346b52284e1455
Russell McWhae <russell.mcwhae@gmail.com>
2023-01-30 20:05:47 -0800

    Two-col layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,16 +6,28 @@
 
 <h1>Gallery</h1>
 
-<div class="grid">
-	{#each images as image}
-		<img src={image.href} alt={image.name} width={200} height={200} />
-	{/each}
+<div class="flex">
+	<div class="sidebar">Filters</div>
+	<div class="grid">
+		{#each images as image}
+			<img src={image.href} alt={image.name} width={200} height={200} />
+		{/each}
+	</div>
 </div>
 
 <style>
 	.grid {
+		flex-grow: 3;
 		display: grid;
 		grid-template-columns: repeat(auto-fill, 200px);
 		grid-gap: 1rem;
+	}
+	.sidebar {
+		flex-grow: 1;
+	}
+	.flex {
+		display: flex;
+		flex-direction: row;
+		gap: 1rem;
 	}
 </style>
